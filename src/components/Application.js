52431db99bfdc1d989ffda0c291848220e3ca280
Jamie Karam <Jamie@Jamies-MacBook-Pro.local>
2020-01-21 19:41:40 -0500

    refactor applications.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -55,13 +55,27 @@ const appointments = [
 
 
 export default function Application(props) {
-  const [days, setDays] = useState([]);
+  
+  
+
+  
+
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
+ 
 
   useEffect(() => {
     axios.get(`http://localhost:8001/api/days`).then((response) => {
     setDays(response.data);
     });
-  }, []);
+  });
 
   return (
     <main className="layout">
@@ -74,12 +88,10 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList 
-            days={days}
-            day={'Tuesday'}
-            setDay={setDays}
+            days={state.days}
+            day={state.day}
+            setDay={setDay}
           />
-
-
 
         </nav>
         <img
