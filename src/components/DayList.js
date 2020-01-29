c1af68fb83aedaf7b406c777a220c00e67a88850
Jamie Karam <Jamie@Jamies-MacBook-Pro.local>
2020-01-29 12:05:53 -0500

    lint and comments

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,16 +4,13 @@ import DayListItem from 'components/DayListItem'
 export default function DayList(props) {
   
   const days = props.days.map(day => {
-    return (
-      
+    return 
         <DayListItem
           key={day.id}
           name={day.name}
           spots={day.spots}
           selected={day.name === props.day}
           setDay={props.setDay} />
-          
-     
     );
   });
   return (
