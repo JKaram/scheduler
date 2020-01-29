ada5caedede55b494934650630fd437d6be0764b
Jamie Karam <Jamie@Jamies-MacBook-Pro.local>
2020-01-29 13:46:19 -0500

    Syntax fix

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,7 +4,8 @@ import DayListItem from 'components/DayListItem'
 export default function DayList(props) {
   
   const days = props.days.map(day => {
-    return 
+    return (
+      
         <DayListItem
           key={day.id}
           name={day.name}
